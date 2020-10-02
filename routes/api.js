9e3a49b325ecb504c3f7e3e469a5f2d2cd731881
Leo <medinayleo@gmail.com>
2020-10-02 16:30:01 -0400

    fixes to api

--- routes/api.js
+++ routes/api.js
@@ -22,7 +22,7 @@ router.get('/venue', async function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization');
-    res.status(200).json(result, { location, category });
+    res.status(200).json(foursquareHelper.venueResultCleaned(result));
   } catch (error) {
     next(error);
   };
