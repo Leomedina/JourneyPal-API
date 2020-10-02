b9869751117befc23aebfea46784bb57998d6400
Leo <medinayleo@gmail.com>
2020-10-02 14:37:07 -0400

    removed a utilitied forr cleaning response

--- routes/api.js
+++ routes/api.js
@@ -18,12 +18,11 @@ const foursquareHelper = require('../helpers/foursquareHelpers');
 router.get('/venue', async function (req, res, next) {
   try { 
     const { location, category } = req.body;
-    console.log(location, category);
     const result = await foursquare.getRecommendations(location, category);
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization');
-    res.status(200).json(foursquareHelper.venueResultCleaned(result));
+    res.status(200).json(result);
   } catch (error) {
     next(error);
   };
