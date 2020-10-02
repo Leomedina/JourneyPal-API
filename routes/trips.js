191ee05716cb21a43c2661c427bfa463a27d41b3
Leo <medinayleo@gmail.com>
2020-10-02 13:28:17 -0400

    more changes

--- routes/trips.js
+++ routes/trips.js
@@ -23,7 +23,17 @@ const { tripTable } = require('../model/models');
 router.get('/', async function (req, res, next) {
   try {
     const results = await tripTable.findAll(req.query);
+    // Website you wish to allow to connect
     res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization');
+
+    //  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
     return res.status(200).json({ "trips": results });
   } catch (error) {
     return next(error);
