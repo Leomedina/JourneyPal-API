b836e29fbdb592cc1e5a031cfee71b3e6c8e3175
Leo <medinayleo@gmail.com>
2020-10-05 13:06:35 -0400

    changes to backend to allow for CORS compliance and for browser to server requests

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ const app = express();
 const usersRoutes = require('./routes/users');
 const tripsRoutes = require('./routes/trips');
 const apiRoutes = require('./routes/api');
-const cors = require("cors");
+const cors = require('cors');
 
 /** Allow JSON body parsing */
 app.use(express.json());
-app.use(cors());
+app.use(cors())
 
 /** Routes */
 app.use('/users', usersRoutes);
--- routes/api.js
+++ routes/api.js
@@ -15,25 +15,22 @@ const router = new express.Router();
 const foursquare = require('../helpers/foursquareAPI');
 const foursquareHelper = require('../helpers/foursquareHelpers');
 
-router.get('/venue', async function (req, res, next) {
+router.post('/venue', async function (req, res, next) {
   try {
     const { location, category } = req.body;
+    if (!location || !category) {
+      console.log("NO LOCATION OR CAT ERROR");
+      throw new error("no location or body")
+    };
     const result = await foursquare.getRecommendations(location, category);
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization');
     res.status(200).json(foursquareHelper.venueResultCleaned(result));
   } catch (error) {
     next(error);
   };
 });
 
-router.get('/eat', async function (req, res, next) {
+router.post('/eat', async function (req, res, next) {
   try {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization');
-
     const { lat, lng, type } = req.body;
     const result = await foursquare.getFoodRec(lat, lng, type);
     res.status(200).json(foursquareHelper.venueResultCleaned(result));
@@ -42,12 +39,8 @@ router.get('/eat', async function (req, res, next) {
   }
 });
 
-router.get('/tips', async function (req, res, next) {
+router.post('/tips', async function (req, res, next) {
   try {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization');
-
     const { venueId } = req.body;
     const result = await foursquare.getTips(venueId);
     res.status(200).json(result);
